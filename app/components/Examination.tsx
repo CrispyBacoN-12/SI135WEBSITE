05512a662003bc83d2c3d89a6eb93871e396a219
CrispyBacoN-12 <kannithifame2@gmail.com>
2025-09-16 20:41:38 +0700

    Update Examination.tsx

--- app/components/Examination.tsx
+++ app/components/Examination.tsx
@@ -109,8 +109,8 @@ export default function Examination() {
               className="w-full p-4 rounded-lg shadow-md border border-rose-500 bg-white"
             >
               <div className="flex items-center justify-between">
-                <h4 className="font-bold justify-start">{e.summary}</h4>
-                <div className="flex flex-col items-end justify-end">
+                <h4 className="font-bold justify-start basis-1/2">{e.summary}</h4>
+                <div className="flex flex-col items-end justify-end basis-1/2">
                   
                   {e.start.dateTime && (
                     <span className="text-red-500 font-semibold text-sm">
