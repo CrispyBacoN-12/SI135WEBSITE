f5d45d28041a56cf581ae6271f98a42cc2948f52
CrispyBacoN-12 <kannithifame2@gmail.com>
2025-09-18 18:40:12 +0000

    fix: update SIID246 page

---
+++ app/student-impact/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+import Image from "next/image";
+
+export default function StudentImpact ()
+{
+    return(
+<div className="flex flex-col gap-y-6 ">
+      {/* Header */}
+      <div className="bg-gradient-to-r from-blue-200 to-emerald-200 shadow-md py-7">
+        <div className="container max-w-screen-2xl mx-auto font-bold text-3xl text-sky-900 px-10 text-center">
+          ช่องทางการติดต่อสอบถาม & แจ้งเรื่องโดยตรง
+        </div>
+      </div>
+      
+      </div>
+      
+    );
+}
