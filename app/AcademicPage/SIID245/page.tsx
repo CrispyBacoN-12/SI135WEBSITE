0dc762da890ae4a2278802d11296b11a54eccc1b
CrispyBacoN-12 <kannithifame2@gmail.com>
2025-10-13 18:02:45 +0700

    Update page.tsx

--- app/AcademicPage/SIID245/page.tsx
+++ app/AcademicPage/SIID245/page.tsx
@@ -222,7 +222,7 @@ if(summaryLink)
                          
 
       {/* Lectures Section */}
-      <div className="container mx-auto px-4 sm:px-6 md:px-8 flex flex-col gap-4 mt-8">
+      <div className=" mx-auto px-4 sm:px-6 md:px-8 flex flex-col gap-4 mt-8">
         {lectures.map((lec, idx) => (
           <LectureCard key={idx} {...lec} /> // แสดงผลข้อมูล lectures
         ))}
