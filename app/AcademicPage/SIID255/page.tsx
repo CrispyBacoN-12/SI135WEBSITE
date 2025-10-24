88dcb0e0ee4086ab6a334101d250e14b0e4f1b96
CrispyBacoN-12 <kannithifame2@gmail.com>
2025-10-24 23:12:01 +0700

    Update page.tsx

--- app/AcademicPage/SIID255/page.tsx
+++ app/AcademicPage/SIID255/page.tsx
@@ -54,6 +54,13 @@ const SIID255 = () => {
   const [summativeList2, setSummativeList2] = useState([]);
   const [summativeList3, setSummativeList3] = useState([]);
 
+  const icon = (
+    <svg className="w-4 h-4 mr-1 inline" fill="currentColor" viewBox="0 0 448 512">
+      <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448
+      c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z" />
+    </svg>
+  );
+
   useEffect(() => {
     fetch(lecUrl)
       .then((r) => r.text())
