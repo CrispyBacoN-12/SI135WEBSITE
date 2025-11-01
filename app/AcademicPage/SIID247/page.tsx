cdf75f159d6dea46afd6d2cdd909d5206f8ae4df
CrispyBacoN-12 <kannithifame2@gmail.com>
2025-11-01 21:11:06 +0700

    Update page.tsx

--- app/AcademicPage/SIID247/page.tsx
+++ app/AcademicPage/SIID247/page.tsx
@@ -228,6 +228,10 @@ const SIID247 = () => {
           <LectureCard key={idx} {...lec} />
         ))}
       </div>
+       <div className="mx-auto">
+        <div className="bg-gradient-to-r from-green-100 to-blue-100 shadow-md py-7 mt-5">
+          <div className="w-full text-left px-4 text-3xl font-bold text-sky-900">CLO assessment</div>
+        </div>
       <div className="mx-auto px-4 sm:px-6 md:px-8 flex flex-col gap-4 mt-8">
          {CLOList.map((lec, idx) => (
             <CLOCard key={idx} {...lec} />
