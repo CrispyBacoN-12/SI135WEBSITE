ba5ea921ebb358d95ecce0754db38416d281a89f
CrispyBacoN-12 <kannithifame2@gmail.com>
2025-12-09 14:33:51 +0700

    Update page.tsx

--- app/AcademicPage/SIID245/page.tsx
+++ app/AcademicPage/SIID245/page.tsx
@@ -184,7 +184,7 @@ if(summaryLink)
           if (CLOLink) {
             handouts.push({
               name: "Question",
-              link: convertDriveLink(s1Link),   // ✅ ใช้ฟังก์ชันแปลง
+              link: convertDriveLink(CLOLink),   // ✅ ใช้ฟังก์ชันแปลง
               icon: (
                 <svg className="w-4 h-4 mr-1 inline" viewBox="0 0 448 512" fill="currentColor">
                   <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z" />
@@ -197,7 +197,7 @@ if(summaryLink)
           if (CLOKeyLink) {
             handouts.push({
               name: "Answer",
-              link: convertDriveLink(s1KeyLink),
+              link: convertDriveLink(CLOKeyLink),
               icon: (
                 <svg className="w-4 h-4 mr-1 inline" viewBox="0 0 448 512" fill="currentColor">
                   <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z" />
