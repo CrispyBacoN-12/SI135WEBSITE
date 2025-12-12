74824ba55822d1ecf8a1199731392058a9ff5a0d
CrispyBacoN-12 <kannithifame2@gmail.com>
2025-12-12 19:31:59 +0700

    Update Providers.tsx

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,5 +1,6 @@
-use client";
+"use client";
 
+import React from "react";
 import { AuthProvider } from "./AuthContext";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
