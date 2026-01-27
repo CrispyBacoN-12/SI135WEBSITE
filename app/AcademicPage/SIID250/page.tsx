2fdeb208eb79714b6111c5d6cedc33949663737c
CrispyBacoN-12 <kannithifame2@gmail.com>
2026-01-27 14:13:03 +0700

    Update page.tsx

--- app/AcademicPage/SIID250/page.tsx
+++ app/AcademicPage/SIID250/page.tsx
@@ -214,7 +214,8 @@ const [CLOASSESSMENT, setCLOASSESSMENT] = useState([]);
                 Canvas
               </a>
                <a
-                href="https://www.youtube.com/playlist?app=desktop&list=PLnWAv0tkYmKGjolHgkric3CR4SZHLydb6"
+                href="https://www.youtube.com/playlist?list=PLnWAv0tkYmKGG8PUm7AzjJGGzCrOqWwtk
+"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="border border-slate-400 text-lg rounded-lg py-1 px-2 w-fit bg-gradient-to-r from-gray-200 to-gray-300 bg-transparent hover:bg-slate-200 transition-colors flex items-center gap-1"
