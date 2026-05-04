b34cc4340e8aa93526e8e9dfc85fec7d6bade96d
CrispyBacoN-12 <kannithifame2@gmail.com>
2026-05-04 17:10:17 +0000

    Update subjectsConfig.js

--- app/data/subjectsConfig.js
+++ app/data/subjectsConfig.js
@@ -14,7 +14,11 @@ export const subjectsConfig = {
     seniors: [
       { code: "SI134", link: "https://siriraj134.com/acad/siid247", linkname: "SI134(247)" },
       { code: "SI133", link: "https://sites.google.com/view/siriraj133official/archives/year-2/siid-247", linkname: "SI133(247)" },
-      // ...
+       { code: 'SI132', link: 'https://sites.google.com/view/siriraj132/archives/year-2/siid247', linkname: 'SI132(247)' },
+    { code: 'SI131', link: 'https://sites.google.com/view/siriraj131official/archives/sophomore/217', linkname: 'SI131(217)' },
+    { code: 'SI130', link: 'https://sites.google.com/view/siriraj130/archives/year-2/217', linkname: 'SI130(217)' },
+    { code: 'SI129', link: 'https://sites.google.com/view/si129academicportal/archive/sophomore/217', linkname: 'SI129(217)' },
+    { code: 'SI128', link: 'https://sites.google.com/view/siriraj128/217', linkname: 'SI128(217)' }
     ],
   }
 }
