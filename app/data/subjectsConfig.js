0f307e2fc256e84f6e38d3081a550e546df1f96f
CrispyBacoN-12 <kannithifame2@gmail.com>
2026-05-04 16:59:43 +0000

    Create subjectsConfig.js

---
+++ app/data/subjectsConfig.js
@@ -0,0 +1,20 @@
+export const subjectsConfig = {
+  SIID247: {
+    code: "SIID247",
+    title: "Circulatory System",
+    semester: "Year 2 Semester 1",
+    image: "/SIID247.png",
+    sheetId: "1BycR2oOEWS5FlGe5KZLcwm6nPuCpHvmn8p-3SCo3rcg",
+    lectureSheet: "247 (CVS)",
+    summativeSheet: "Summative2",
+    cloSheet: "CLO",
+    lectureLimit: 22,
+    canvasLink: "https://sirirajcanvas.instructure.com/courses/1078",
+    youtubeLink: "https://www.youtube.com/playlist?list=PLnWAv0tkYmKGjolHgkric3CR4SZHLydb6",
+    seniors: [
+      { code: "SI134", link: "https://siriraj134.com/acad/siid247", linkname: "SI134(247)" },
+      { code: "SI133", link: "https://sites.google.com/view/siriraj133official/archives/year-2/siid-247", linkname: "SI133(247)" },
+      // ...
+    ],
+  }
+}
